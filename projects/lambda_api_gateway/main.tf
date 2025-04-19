373d2f233d557dab1c94ebccca6e74143590eb20
Emerson Maciel <59801743+Emersoft76@users.noreply.github.com>
2025-04-19 18:39:30 +0100

    main.tf

---
+++ projects/lambda_api_gateway/main.tf
@@ -0,0 +1,45 @@
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "lambda.amazonaws.com" },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_lambda_function" "api" {
+  filename         = "lambda_function_payload.zip"
+  function_name    = "HelloLambda"
+  role             = aws_iam_role.lambda_exec.arn
+  handler          = "lambda_function.handler"
+  runtime          = "python3.9"
+  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+}
+
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "serverless-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id             = aws_apigatewayv2_api.http_api.id
+  integration_type   = "AWS_PROXY"
+  integration_uri    = aws_lambda_function.api.invoke_arn
+  integration_method = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "default" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "GET /"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}

9d78c617115c4b609866bbab7588fbeee9877963
Emerson Maciel <59801743+Emersoft76@users.noreply.github.com>
2025-04-21 14:49:13 +0100

    variables.tf

---
+++ projects/cloudwatch_logs_alerts/variables.tf
@@ -0,0 +1,3 @@
+variable "region"       { default = "us-east-1" }
+variable "instance_id"  { description = "ID da instância EC2 monitorada" }
+variable "alert_email"  { description = "E-mail para receber alertas" }
